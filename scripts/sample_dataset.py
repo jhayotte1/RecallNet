7d8ab03e3653260f868cfe17791fa0c232123904
jhayotte1 <jacques.hayotte2004@gmail.com>
2026-07-08 13:37:39 +0200

    update sample

--- scripts/sample_dataset.py
+++ scripts/sample_dataset.py
@@ -9,8 +9,29 @@ DATA_DIR = Path(__file__).parent.parent / "data"
 def sample_dataset(df: pd.DataFrame, size=SAMPLE_SIZE):
     return(df.sample(size))
 
+def sample_predicate(df: pd.DataFrame, size=SAMPLE_SIZE, predicate: str=""):
+    df_sampled = df[df["predicate"] == predicate].copy()
+    if len(df_sampled) == 0:
+        return None
+    df_sampled = df_sampled.sample(size)
+    return df_sampled
+
+
 if __name__ == "__main__":
     df_quasi = load_quasimodo()
-    df_sampled = sample_dataset(df_quasi, size=SAMPLE_SIZE)
-    df_sampled.to_csv(f"{DATA_DIR}/quasi_test_100_sample.csv")
+    #df_sampled = sample_dataset(df_quasi, size=SAMPLE_SIZE)
+    predicate_list1 = ["causes", "causes desire", "created by", "defined as"]
+    predicate_list2 = ["desires", "distinct from", "has a", "has subevent"]
+    predicate_list3 = ["has first subevent", "has last subevent", "has prerequisite", "motivated by goal"]
+    predicate_list4 = ["made of", "manner of", "part of", "receives action", "used for"]
+    predicate_list5 = ["instance of", "entails"]
+    for predicate in predicate_list5:
+        df_sampled = sample_predicate(df_quasi, size=SAMPLE_SIZE, predicate=predicate)
+        if df_sampled is not None:
+            predicate = predicate.strip().replace(" ", "")
+            df_sampled.to_csv(f"{DATA_DIR}/quasi_sample_{predicate}.csv", index=False)
+            print(f"Quasimodo dataset sampled at sample_size = {SAMPLE_SIZE} and predicate = '{predicate}'")
+            print(f"Saved at: {DATA_DIR}/quasi_sample_{predicate}.csv")
+        else:
+            print(f"No triple with predicate = '{predicate}'")
 

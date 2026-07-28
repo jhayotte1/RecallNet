0dd85a981a5c94c79f7ea8f1013d379fc48151f0
jhayotte1 <jacques.hayotte2004@gmail.com>
2026-07-28 12:59:23 +0000

    move run bash

--- scripts/vLLM_pipeline/config.py
+++ scripts/vLLM_pipeline/config.py
@@ -4,7 +4,7 @@ from pathlib import Path
 PACKAGE_DIR = Path(__file__).parent
 
 
-VLLM_MODEL = os.environ.get("RECALLNET_VLLM_MODEL", "meta-llama/Llama-3.1-8B-Instruct")
+VLLM_MODEL = os.environ.get("RECALLNET_VLLM_MODEL", "~/RecallNet/models/meta-Llama-3.1-8B-Instruct")
 
 MODEL_NAME_LIGHT = os.environ.get("RECALLNET_MODEL_LIGHT", "llama3.1:8b-fp8")
 
@@ -15,8 +15,7 @@ MAX_MODEL_LEN = int(os.environ.get("RECALLNET_VLLM_MAX_LEN", "8192"))
 BATCH_SIZE = int(os.environ.get("RECALLNET_BATCH_SIZE", "10"))
 TEMPERATURE = float(os.environ.get("RECALLNET_TEMPERATURE", "0"))
 
-MAX_TOKENS = int(os.environ.get("RECALLNET_VLLM_MAX_TOKENS",
-                                str(150 * BATCH_SIZE)))
+MAX_TOKENS = int(os.environ.get("RECALLNET_VLLM_MAX_TOKENS", str(150 * BATCH_SIZE)))
 
 SYSTEM_PROMPT_PATH = PACKAGE_DIR / os.environ.get("RECALLNET_PROMPT", "prompt.txt")
 PREDICATE_REG_PATH = PACKAGE_DIR / os.environ.get("RECALLNET_REGISTRY", "predicate_registry.json")
